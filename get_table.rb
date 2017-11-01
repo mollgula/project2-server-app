15150fde4b7ef1cf8fcbf0810b06b1aa3e8786c9
佐藤嘉威 <kd1224759@st.kobedenshi.ac.jp>
2017-11-01 02:55:25 +0000

    11/1 授業

--- get_table.rb
+++ get_table.rb
@@ -10,6 +10,14 @@ doc = Nokogiri::HTML(html)
 # binding.pry
 
 # 例：h2要素のみを抜き出す
-doc.css('h2').each do |node|
-  puts node.text
+#doc.css('h2').each do |node|
+  #puts node.text
+#end
+
+doc.xpath("//*[@id=\"Production_Releases\"]").each do |title|
+ puts title.text
+  end
+  
+doc.xpath('//*[@id="content"]/div[3]').each do |table|
+  print table.text.ljust(15)
 end
